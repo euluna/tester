6c658993631e9c44b358e73ce09fa4543193d166
Euluna Gotami <eulunag@gmail.com>
2024-12-06 21:08:16 -0800

    Reupload

--- config.js
+++ config.js
@@ -1,10 +1,27 @@
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
-}); 
+const config = {
+    API_KEY: 'sk-YiLu67441e9ba21997759',
+    PLANT_OF_DAY_ID: (() => {
+        // Get current date in YYYYMMDD format
+        const today = new Date();
+        const dateString = today.getFullYear().toString() + 
+                          (today.getMonth() + 1).toString().padStart(2, '0') + 
+                          today.getDate().toString().padStart(2, '0');
+        
+        // Convert date string to a number and use it as a seed
+        let seed = parseInt(dateString);
+        
+        // Use a larger multiplier to get better distribution
+        seed = seed * 1234567;
+        
+        // Use modulo to get a number between 0 and 10101, then add 1
+        const randomNum = (seed % 10102) + 1;
+        
+        return randomNum;
+    })(),
+    
+    // Google API credentials
+    GOOGLE_CLIENT_ID: '',
+    GOOGLE_API_KEY: '',
+    GOOGLE_DISCOVERY_DOC: 'https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest',
+    GOOGLE_SCOPES: 'https://www.googleapis.com/auth/tasks'
+};
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/setup/jest.setup.js'],
-  testTimeout: 30000,
-  verbose: true
-};
