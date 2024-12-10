3b6cf92cc5eda66475e7215386ccc8cbe2364efb
Euluna Gotami <131542211+euluna@users.noreply.github.com>
2024-12-10 00:58:15 -0800

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const config = {
         // Use a larger multiplier to get better distribution
         seed = seed * 1234567;
         
-        // Use modulo to get a number between 0 and 10101, then add 1
-        const randomNum = (seed % 10102) + 1;
+        // Use modulo to get a number between 0 and 3000, then add 1
+        const randomNum = (seed % 2999) + 1;
         
         return randomNum;
     })(),
