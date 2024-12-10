ad9dfb2e8a25a6e01bf6361404edac115f6739cf
Euluna Gotami <131542211+euluna@users.noreply.github.com>
2024-12-09 23:43:43 -0800

    Replaced with restricted keys

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ const config = {
     })(),
     
     // Google API credentials
-    GOOGLE_CLIENT_ID: '1092792530032-1rb8apfgl9ndhvuteuq4sr3kcfph3smb.apps.googleusercontent.com',
-    GOOGLE_API_KEY: 'AIzaSyAQ_85hoZstHGA36ydO-R7WR3RjmCPa-WY',
+    GOOGLE_CLIENT_ID: '1092792530032-3mkc2c7t3r8poo8k4muiqutscpheov5h.apps.googleusercontent.com',
+    GOOGLE_API_KEY: 'AIzaSyCdQQhHZQnxAH70wBOh2dk7ZoM1V4Y9bc8',
     GOOGLE_DISCOVERY_DOC: 'https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest',
     GOOGLE_SCOPES: 'https://www.googleapis.com/auth/tasks'
 };
